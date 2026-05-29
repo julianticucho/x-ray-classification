8c891de00a94eba061f0bdb8ca188f86e776779f
julian <julian.proboste@ug.uchile.cl>
2026-05-28 20:34:36 -0400

    mas imagenes

--- scripts/plot_roc.py
+++ scripts/plot_roc.py
@@ -39,6 +39,10 @@ if __name__ == '__main__':
     #     output_path='results/roc/full_data_binary_binary_densenet_binary_15epochs.pdf'
     # )
     plot_roc_and_save(
-        model_path='results/models/full_data_effusion_effusion_densenet_binary_15epochs.pth',
-        output_path='results/roc/full_data_effusion_effusion_densenet_binary_15epochs.pdf'
+        model_path='results/models/full_data_effusion_effusion_low_res_densenet_binary_1epoch.pth',
+        output_path='results/roc/full_data_effusion_effusion_low_res_densenet_binary_1epoch.pdf'
     )
+    # plot_roc_and_save(
+    #     model_path='results/models/full_data_effusion_effusion_densenet_binary_15epochs.pth',
+    #     output_path='results/roc/full_data_effusion_effusion_densenet_binary_15epochs.pdf'
+    # )
